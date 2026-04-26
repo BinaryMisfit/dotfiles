bf385808ba3630b5cc50243a31c06ae8b7436052
Willie Roberts <diagoza@me.com>
2026-04-26 16:22:32 +0200

    Fixed typo

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -47,7 +47,7 @@ if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
 
 Section "core tools"
 
-$Tools = @("git", "chezmoi", "age", "mise", "oh-my-posh", "openssh", "minwg")
+$Tools = @("git", "chezmoi", "age", "mise", "oh-my-posh", "openssh", "mingw")
 
 foreach ($Tool in $Tools) {
     Info "ensuring $Tool"
