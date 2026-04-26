02a81d8245ad11fd5c32f89c3cae1d4a10bfb9c6
Willie Roberts <diagoza@me.com>
2026-04-26 16:04:47 +0200

    Fix treesitter

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,15 @@
-local treesitter = require("nvim-treesitter")
-
-treesitter.setup({
-	install_dir = vim.fn.stdpath("data") .. "/site",
-})
-
-local parsers = {
-	"lua",
-	"vim",
-	"bash",
-	"json",
-	"yaml",
-	"toml",
-	"markdown",
-	"dockerfile",
-}
-
-pcall(treesitter.install, parsers)
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = parsers,
-	callback = function()
-		pcall(vim.treesitter.start)
-	end,
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"lua",
+		"vim",
+		"bash",
+		"json",
+		"yaml",
+		"toml",
+		"markdown",
+		"dockerfile",
+	},
+	highlight = {
+		enable = true,
+	},
 })
