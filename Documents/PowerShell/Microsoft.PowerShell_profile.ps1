68fa090761a91bacf6d399d5023edaaba8b0ea5c
Willie Roberts <diagoza@me.com>
2026-04-27 09:09:50 +0200

    Updated Profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,15 @@
 [Console]::InputEncoding = [Console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
+$profileDir = Split-Path -Parent $PROFILE
+
 # Token loader
-$envLoader = "$HOME\Documents\PowerShell\env.ps1"
+$envLoader = Join-Path $profileDir "env.ps1"
 if (Test-Path $envLoader) {
     . $envLoader
 }
 
 # oh-my-posh
-$ompTheme = "$HOME\Documents\PowerShell\themes\pwsh10k.omp.json"
+$ompTheme = Join-Path $profileDir "themes\pwsh10k.omp.json"
 
 if ((Get-Command oh-my-posh -ErrorAction SilentlyContinue) -and (Test-Path $ompTheme)) {
     oh-my-posh init pwsh --config $ompTheme | Invoke-Expression
@@ -21,11 +23,14 @@ if (Get-Command mise -ErrorAction SilentlyContinue) {
 # basic aliases
 Set-Alias ll Get-ChildItem
 Set-Alias g git
-Set-Alias vim nvim
-Set-Alias vi nvim
+
+if (Get-Command nvim -ErrorAction SilentlyContinue) {
+    Set-Alias vim nvim
+    Set-Alias vi nvim
+}
 
 # Keymaps
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
