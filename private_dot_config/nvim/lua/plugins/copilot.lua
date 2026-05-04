1370f309d6b787e884d38b5a2966fb25257a6aa8
Willie Roberts <diagoza@me.com>
2026-05-04 12:23:38 +0200

    Added copilot to neovim

---
+++ private_dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,15 @@
+return {
+	suggestion = {
+		enabled = true,
+		auto_trigger = false,
+		keymap = {
+			accept = "<M-l>",
+			next = "<M-]>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+	},
+	panel = {
+		enabled = false,
+	},
+}
